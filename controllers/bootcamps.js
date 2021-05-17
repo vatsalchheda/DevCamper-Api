18e029e7682c2b9f150166e2bbd33ad5f5a0847c
Vatsal Chheda <vatsalchheda007@gmail.com>
2021-05-17 15:11:42 +0530

    Error Handlers

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,43 +1,83 @@
+const { Error } = require('mongoose');
+const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 // @desc        Get All Bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
 
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+  res
+    .status(200)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
 // @desc        Get a single Bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Public
 
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` });
-};
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+  if (!bootcamp) {
+    next(
+      new ErrorResponse(
+        `Bootcamp not found with the id of ${req.params.id}`,
+        404
+      )
+    );
+  }
+  res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc        Create a Bootcamp
 // @route       POST /api/v1/bootcamps/
 // @access      Private
 
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-};
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({ success: true, data: bootcamp });
+});
 
 // @desc        Update a Bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Private
 
-exports.updateBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update a bootcamp ${req.params.id}` });
-};
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!bootcamp) {
+    next(
+      new ErrorResponse(
+        `Bootcamp not found with the id of ${req.params.id}`,
+        404
+      )
+    );
+  }
+  res.status(200).json({
+    success: true,
+    msg: `Updated bootcamp ${req.params.id}`,
+    data: bootcamp,
+  });
+});
 
 // @desc        Delete a single Bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Private
 
-exports.deleteBootcamp = (req, res, next) => {
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  if (!bootcamp) {
+    next(
+      new ErrorResponse(
+        `Bootcamp not found with the id of ${req.params.id}`,
+        404
+      )
+    );
+  }
   res
     .status(200)
     .json({ success: true, msg: `Delete a bootcamp ${req.params.id}` });
-};
+});
--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route Files
-const bootcamps = require('./routes/bootcamps');
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 // LOAD ENV VARS
 dotenv.config({ path: './config/config.env' });
 
+// Connect to DB
+connectDB();
+
+// Route Files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
+// Body Parser
+app.use(express.json());
+
+// DEV Logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 // Mount ROuters
 app.use('/api/v1/bootcamps', bootcamps);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App listening in ${process.env.NODE_ENV} mode on port ${PORT}!`);
+});
+
+// Handle Unhandled promise Rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close Server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
 });
